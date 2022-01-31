8b731bc4fb07d2edd4462c539aa2c2d1f2e1e582
Craig Hobbs <craigahobbs@gmail.com>
2022-01-31 15:46:42 -0800

    initial commit

---
+++ test/testParser.js
@@ -0,0 +1,361 @@
+// Licensed under the MIT License
+// https://github.com/craigahobbs/markdown-charts/blob/main/LICENSE
+
+/* eslint-disable id-length */
+
+import {evaluateExpression, executeScript, validateExpression, validateScript} from '../lib/runtime.js';
+import {parseExpression, parseScript} from '../lib/parser.js';
+import test from 'ava';
+test('parseScript, jumpif', (t) => {
+    const script = validateScript(parseScript(`
+n = 10
+i = 0
+a = 0
+b = 1
+
+fib:
+    jumpif (i >= [n]) fibend
+    tmp = b
+    b = a + b
+    a = tmp
+    i = i + 1
+    jump fib
+fibend:
+
+return a
+`));
+    t.deepEqual(script, {
+        'statements': [
+            {'assignment': {'name': 'n', 'expression': {'number': 10}}},
+            {'assignment': {'name': 'i', 'expression': {'number': 0}}},
+            {'assignment': {'name': 'a', 'expression': {'number': 0}}},
+            {'assignment': {'name': 'b', 'expression': {'number': 1}}},
+            {'label': 'fib'},
+            {
+                'jump': {
+                    'label': 'fibend',
+                    'expression': {'binary': {'operator': '>=', 'left': {'variable': 'i'}, 'right': {'variable': 'n'}}}
+                }
+            },
+            {'assignment': {'name': 'tmp', 'expression': {'variable': 'b'}}},
+            {
+                'assignment': {
+                    'name': 'b',
+                    'expression': {'binary': {'operator': '+', 'left': {'variable': 'a'}, 'right': {'variable': 'b'}}}
+                }
+            },
+            {'assignment': {'name': 'a', 'expression': {'variable': 'tmp'}}},
+            {
+                'assignment': {
+                    'name': 'i',
+                    'expression': {'binary': {'operator': '+', 'left': {'variable': 'i'}, 'right': {'number': 1}}}
+                }
+            },
+            {'jump': {'label': 'fib'}},
+            {'label': 'fibend'},
+            {'return': {'variable': 'a'}}
+        ]
+    });
+    t.is(executeScript(script), 55);
+});
+
+
+test('parseExpression', (t) => {
+    const expr = parseExpression('7 + 3 * 5');
+    t.deepEqual(validateExpression(expr), {
+        'binary': {
+            'operator': '+',
+            'left': {'number': 7},
+            'right': {
+                'binary': {
+                    'operator': '*',
+                    'left': {'number': 3},
+                    'right': {'number': 5}
+                }
+            }
+        }
+    });
+    t.is(evaluateExpression(expr), 7 + 3 * 5);
+});
+
+
+test('parseExpression, operator precedence', (t) => {
+    const expr = parseExpression('7 * 3 + 5');
+    t.deepEqual(validateExpression(expr), {
+        'binary': {
+            'operator': '+',
+            'left': {
+                'binary': {
+                    'operator': '*',
+                    'left': {'number': 7},
+                    'right': {'number': 3}
+                }
+            },
+            'right': {'number': 5}
+        }
+    });
+    t.is(evaluateExpression(expr), 7 * 3 + 5);
+});
+
+
+test('parseExpression, operator precedence 2', (t) => {
+    const expr = parseExpression('2 * 3 + 4 - 1');
+    t.deepEqual(validateExpression(expr), {
+        'binary': {
+            'operator': '-',
+            'left': {
+                'binary': {
+                    'operator': '+',
+                    'left': {
+                        'binary': {
+                            'operator': '*',
+                            'left': {'number': 2},
+                            'right': {'number': 3}
+                        }
+                    },
+                    'right': {'number': 4}
+                }
+            },
+            'right': {'number': 1}
+        }
+    });
+    t.is(evaluateExpression(expr), 2 * 3 + 4 - 1);
+});
+
+
+test('parseExpression, operator precedence 3', (t) => {
+    const expr = parseExpression('2 + 3 + 4 - 1');
+    t.deepEqual(validateExpression(expr), {
+        'binary': {
+            'operator': '-',
+            'left': {
+                'binary': {
+                    'operator': '+',
+                    'left': {
+                        'binary': {
+                            'operator': '+',
+                            'left': {'number': 2},
+                            'right': {'number': 3}
+                        }
+                    },
+                    'right': {'number': 4}
+                }
+            },
+            'right': {'number': 1}
+        }
+    });
+    t.is(evaluateExpression(expr), 2 + 3 + 4 - 1);
+});
+
+
+test('parseExpression, operator precedence 4', (t) => {
+    const expr = parseExpression('1 - 2 + 3 + 4 + 5 * 6');
+    t.deepEqual(validateExpression(expr), {
+        'binary': {
+            'operator': '+',
+            'left': {
+                'binary': {
+                    'operator': '+',
+                    'left': {
+                        'binary': {
+                            'operator': '+',
+                            'left': {
+                                'binary': {
+                                    'operator': '-',
+                                    'left': {'number': 1},
+                                    'right': {'number': 2}
+                                }
+                            },
+                            'right': {'number': 3}
+                        }
+                    },
+                    'right': {'number': 4}
+                }
+            },
+            'right': {
+                'binary': {
+                    'operator': '*',
+                    'left': {'number': 5},
+                    'right': {'number': 6}
+                }
+            }
+        }
+    });
+    t.is(evaluateExpression(expr), 1 - 2 + 3 + 4 + 5 * 6);
+});
+
+
+test('parseExpression, operator precedence 5', (t) => {
+    const expr = parseExpression('1 + 2 * 5 / 2');
+    t.deepEqual(expr, {
+        'binary': {
+            'operator': '+',
+            'left': {'number': 1},
+            'right': {
+                'binary': {
+                    'operator': '/',
+                    'left': {
+                        'binary': {
+                            'operator': '*',
+                            'left': {'number': 2},
+                            'right': {'number': 5}
+                        }
+                    },
+                    'right': {'number': 2}
+                }
+            }
+        }
+    });
+    t.is(evaluateExpression(expr), 1 + 2 * 5 / 2);
+});
+
+
+test('parseExpression, operator precedence 6', (t) => {
+    const expr = parseExpression('1 + 2 / 5 * 2');
+    t.deepEqual(expr, {
+        'binary': {
+            'operator': '+',
+            'left': {'number': 1},
+            'right': {
+                'binary': {
+                    'operator': '*',
+                    'left': {
+                        'binary': {
+                            'operator': '/',
+                            'left': {'number': 2},
+                            'right': {'number': 5}
+                        }
+                    },
+                    'right': {'number': 2}
+                }
+            }
+        }
+    });
+    t.is(evaluateExpression(expr), 1 + 2 / 5 * 2);
+});
+
+
+test('parseExpression, operator precedence 7', (t) => {
+    const expr = parseExpression('1 + 2 / 3 / 4 * 5');
+    t.deepEqual(expr, {
+        'binary': {
+            'operator': '+',
+            'left': {'number': 1},
+            'right': {
+                'binary': {
+                    'operator': '*',
+                    'left': {
+                        'binary': {
+                            'operator': '/',
+                            'left': {
+                                'binary': {
+                                    'operator': '/',
+                                    'left': {'number': 2},
+                                    'right': {'number': 3}
+                                }
+                            },
+                            'right': {'number': 4}
+                        }
+                    },
+                    'right': {'number': 5}
+                }
+            }
+        }
+    });
+    t.is(evaluateExpression(expr), 1 + 2 / 3 / 4 * 5);
+});
+
+
+test('parseExpression, operator precedence 8', (t) => {
+    const expr = parseExpression('1 >= 2 && 3 < 4 - 5');
+    t.deepEqual(expr, {
+        'binary': {
+            'operator': '&&',
+            'left': {
+                'binary': {
+                    'operator': '>=',
+                    'left': {'number': 1},
+                    'right': {'number': 2}
+                }
+            },
+            'right': {
+                'binary': {
+                    'operator': '<',
+                    'left': {'number': 3},
+                    'right': {
+                        'binary': {
+                            'operator': '-',
+                            'left': {'number': 4},
+                            'right': {'number': 5}
+                        }
+                    }
+                }
+            }
+        }
+    });
+    t.is(evaluateExpression(expr), false);
+});
+
+
+test('parseExpression, group', (t) => {
+    const expr = parseExpression('(7 + 3) * 5');
+    t.deepEqual(validateExpression(expr), {
+        'binary': {
+            'operator': '*',
+            'left': {
+                'group': {
+                    'binary': {
+                        'operator': '+',
+                        'left': {'number': 7},
+                        'right': {'number': 3}
+                    }
+                }
+            },
+            'right': {'number': 5}
+        }
+    });
+    t.is(evaluateExpression(expr), 50);
+});
+
+
+test('parseExpression, group nested', (t) => {
+    const expr = parseExpression('(1 + (2))');
+    t.deepEqual(validateExpression(expr), {
+        'group': {
+            'binary': {
+                'operator': '+',
+                'left': {'number': 1},
+                'right': {'group': {'number': 2}}
+            }
+        }
+    });
+    t.is(evaluateExpression(expr), 3);
+});
+
+
+test('parseExpression, string literal', (t) => {
+    const expr = parseExpression("'abc'");
+    t.deepEqual(validateExpression(expr), {'string': 'abc'});
+    t.is(evaluateExpression(expr), 'abc');
+});
+
+
+test('parseExpression, string literal escapes', (t) => {
+    const expr = parseExpression("'ab \\'c\\' d\\\\e \\f'");
+    t.deepEqual(validateExpression(expr), {'string': "ab 'c' d\\e \\f"});
+    t.is(evaluateExpression(expr), "ab 'c' d\\e \\f");
+});
+
+
+test('parseExpression, string literal double-quote', (t) => {
+    const expr = parseExpression('"abc"');
+    t.deepEqual(validateExpression(expr), {'string': 'abc'});
+    t.is(evaluateExpression(expr), 'abc');
+});
+
+
+test('parseExpression, string literal double-quote escapes', (t) => {
+    const expr = parseExpression('"ab \\"c\\" d\\\\e \\f"');
+    t.deepEqual(validateExpression(expr), {'string': 'ab "c" d\\e \\f'});
+    t.is(evaluateExpression(expr), 'ab "c" d\\e \\f');
+});
---
+++ test/testRuntime.js
@@ -0,0 +1,164 @@
+// Licensed under the MIT License
+// https://github.com/craigahobbs/markdown-charts/blob/main/LICENSE
+
+/* eslint-disable id-length */
+
+import {evaluateExpression, executeScript, validateExpression, validateScript} from '../lib/runtime.js';
+import test from 'ava';
+
+
+test('executeScript', (t) => {
+    const script = validateScript({
+        'statements': [
+            {'assignment': {'name': 'a', 'expression': {'number': 5}}},
+            {'assignment': {'name': 'b', 'expression': {'number': 7}}},
+            {'return': {'binary': {'operator': '+', 'left': {'variable': 'a'}, 'right': {'variable': 'b'}}}}
+        ]
+    });
+    t.is(executeScript(script), 12);
+});
+
+
+test('executeScript, function', (t) => {
+    const script = validateScript({
+        'statements': [
+            {
+                'function': {
+                    'name': 'multiplyNumbers',
+                    'arguments': ['a', 'b'],
+                    'statements': [
+                        {'return': {'binary': {'operator': '*', 'left': {'variable': 'a'}, 'right': {'variable': 'b'}}}}
+                    ]
+                }
+            },
+            {'return': {'function': {'name': 'multiplyNumbers', 'arguments': [{'number': 5}, {'number': 7}]}}}
+        ]
+    });
+    t.is(executeScript(script), 35);
+});
+
+
+test('executeScript, jump', (t) => {
+    const script = validateScript({
+        'statements': [
+            {'assignment': {'name': 'a', 'expression': {'number': 5}}},
+            {'jump': {'label': 'lab2'}},
+            {'label': 'lab'},
+            {'assignment': {'name': 'a', 'expression': {'number': 6}}},
+            {'jump': {'label': 'lab3'}},
+            {'label': 'lab2'},
+            {'assignment': {'name': 'a', 'expression': {'number': 7}}},
+            {'jump': {'label': 'lab'}},
+            {'label': 'lab3'},
+            {'return': {'variable': 'a'}}
+        ]
+    });
+    t.is(executeScript(script), 6);
+});
+
+
+test('executeScript, jumpif', (t) => {
+    const script = validateScript({
+        'statements': [
+            {'assignment': {'name': 'n', 'expression': {'number': 10}}},
+            {'assignment': {'name': 'i', 'expression': {'number': 0}}},
+            {'assignment': {'name': 'a', 'expression': {'number': 0}}},
+            {'assignment': {'name': 'b', 'expression': {'number': 1}}},
+            {'label': 'fib'},
+            {'jump': {
+                'label': 'fibend',
+                'expression': {'binary': {'operator': '>=', 'left': {'variable': 'i'}, 'right': {'variable': 'n'}}}
+            }},
+            {'assignment': {'name': 'tmp', 'expression': {'variable': 'b'}}},
+            {'assignment': {
+                'name': 'b',
+                'expression': {'binary': {'operator': '+', 'left': {'variable': 'a'}, 'right': {'variable': 'b'}}}
+            }},
+            {'assignment': {'name': 'a', 'expression': {'variable': 'tmp'}}},
+            {'assignment': {'name': 'i', 'expression': {'binary': {'operator': '+', 'left': {'variable': 'i'}, 'right': {'number': 1}}}}},
+            {'jump': {'label': 'fib'}},
+            {'label': 'fibend'},
+            {'return': {'variable': 'a'}}
+        ]
+    });
+    t.is(executeScript(script), 55);
+});
+
+
+test('executeScript, maxStatements', (t) => {
+    const script = validateScript({
+        'statements': [
+            {'expression': {'variable': 'a'}},
+            {'expression': {'variable': 'b'}},
+            {'expression': {'variable': 'c'}}
+        ]
+    });
+    let errorMessage = null;
+    try {
+        executeScript(script, {}, 2);
+    } catch ({message}) {
+        errorMessage = message;
+    }
+    t.is(errorMessage, 'Exceeded maximum script statements (2)');
+    t.is(executeScript(script, {}, 3), null);
+    t.is(executeScript(script, {}, 0), null);
+});
+
+
+test('evaluateExpression', (t) => {
+    const calc = validateExpression({
+        'binary': {
+            'operator': '+',
+            'left': {'number': 7},
+            'right': {
+                'binary': {
+                    'operator': '*',
+                    'left': {'number': 3},
+                    'right': {
+                        'function': {
+                            'name': 'ceil',
+                            'arguments': [
+                                {'variable': 'varName'}
+                            ]
+                        }
+                    }
+                }
+            }
+        }
+    });
+    const variables = {'varName': 4};
+    t.is(evaluateExpression(calc, variables), 19);
+});
+
+
+test('evaluateExpression, function variable', (t) => {
+    const calc = validateExpression({
+        'function': {
+            'name': 'fnName',
+            'arguments': [
+                {'number': 3}
+            ]
+        }
+    });
+    const variables = {'fnName': ([number]) => 2 * number};
+    t.is(evaluateExpression(calc, variables), 6);
+});
+
+
+test('evaluateExpression, function unknown', (t) => {
+    const calc = validateExpression({
+        'function': {
+            'name': 'fnUnknown',
+            'arguments': []
+        }
+    });
+    const variables = {};
+    const getVariable = (name) => (name in variables ? variables[name] : null);
+    let errorMessage = null;
+    try {
+        evaluateExpression(calc, getVariable);
+    } catch ({message}) {
+        errorMessage = message;
+    }
+    t.is(errorMessage, 'Undefined function "fnUnknown"');
+});
