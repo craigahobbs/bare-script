c044b0311ae3a4d9149d293bec90e7e3a92f7222
Craig Hobbs <craigahobbs@gmail.com>
2022-06-28 16:34:00 -0700

    improved parser errors, parser unit tests

--- test/testParser.js
+++ test/testParser.js
@@ -3,12 +3,55 @@
 
 /* eslint-disable id-length */
 
-import {evaluateExpression, executeScript} from '../lib/runtime.js';
 import {parseExpression, parseScript} from '../lib/parser.js';
 import {validateExpression, validateScript} from '../lib/model.js';
 import test from 'ava';
-test('parseScript, jumpif', (t) => {
-    const script = validateScript(parseScript(`
+
+
+test('parseScript, array input', (t) => {
+    const script = validateScript(parseScript([
+        'a = arrayNew( \\',
+        '    1,\\',
+        `\
+    2 \
+)
+`
+    ]));
+    t.deepEqual(script, {
+        'statements': [
+            {
+                'assign': {
+                    'name': 'a',
+                    'expr': {'function': {'name': 'arrayNew', 'args': [{'number': 1}, {'number': 2}]}}
+                }
+            }
+        ]
+    });
+});
+
+
+test('parseScript, line continuation', (t) => {
+    const script = validateScript(parseScript(`\
+a = arrayNew( \
+    1, \
+    2 \
+)
+`));
+    t.deepEqual(script, {
+        'statements': [
+            {
+                'assign': {
+                    'name': 'a',
+                    'expr': {'function': {'name': 'arrayNew', 'args': [{'number': 1}, {'number': 2}]}}
+                }
+            }
+        ]
+    });
+});
+
+
+test('parseScript, jumpif statement', (t) => {
+    const script = validateScript(parseScript(`\
 n = 10
 i = 0
 a = 0
@@ -57,7 +100,214 @@ return a
             {'return': {'expr': {'variable': 'a'}}}
         ]
     });
-    t.is(executeScript(script), 55);
+});
+
+
+test('parseScript, function statement', (t) => {
+    const script = validateScript(parseScript(`\
+function addNumbers(a, b)
+    return a + b
+endfunction
+`));
+    t.deepEqual(script, {
+        'statements': [
+            {
+                'function': {
+                    'name': 'addNumbers',
+                    'args': ['a', 'b'],
+                    'statements': [
+                        {
+                            'return': {
+                                'expr': {'binary': {'op': '+', 'left': {'variable': 'a'}, 'right': {'variable': 'b'}}}
+                            }
+                        }
+                    ]
+                }
+            }
+        ]
+    });
+});
+
+
+test('parseScript, async function statement', (t) => {
+    const script = validateScript(parseScript(`\
+async function fetchURL(url)
+    return fetch(url)
+endfunction
+`));
+    t.deepEqual(script, {
+        'statements': [
+            {
+                'function': {
+                    'async': true,
+                    'name': 'fetchURL',
+                    'args': ['url'],
+                    'statements': [
+                        {'return': {'expr': {'function': {'name': 'fetch', 'args': [{'variable': 'url'}]}}}}
+                    ]
+                }
+            }
+        ]
+    });
+});
+
+
+test('parseScript, include statement', (t) => {
+    const script = validateScript(parseScript(`\
+include 'fi\\'le.mds'
+`));
+    t.deepEqual(script, {
+        'statements': [
+            {'include': {'url': "fi'le.mds"}}
+        ]
+    });
+});
+
+
+test('parseScript, include statement, double-quotes', (t) => {
+    const script = validateScript(parseScript(`\
+include "fi\\"le.mds"
+`));
+    t.deepEqual(script, {
+        'statements': [
+            {'include': {'url': 'fi"le.mds'}}
+        ]
+    });
+});
+
+
+test('parseScript, expression statement syntax error', (t) => {
+    let error = null;
+    try {
+        validateScript(parseScript(`\
+a = 0
+b = 1
+foo \
+bar
+c = 2
+`));
+    } catch (errorTest) {
+        error = errorTest;
+    }
+    t.is(error.message, `\
+Syntax error, line number 3:
+foo bar
+   ^
+`);
+    t.is(error.error, 'Syntax error');
+    t.is(error.line, 'foo bar');
+    t.is(error.columnNumber, 4);
+    t.is(error.lineNumber, 3);
+});
+
+
+test('parseScript, assignment statement expression syntax error', (t) => {
+    let error = null;
+    try {
+        validateScript(parseScript(`\
+a = 0
+b = 1 + foo bar
+`));
+    } catch (errorTest) {
+        error = errorTest;
+    }
+    t.is(error.message, `\
+Syntax error, line number 2:
+b = 1 + foo bar
+           ^
+`);
+    t.is(error.error, 'Syntax error');
+    t.is(error.line, 'b = 1 + foo bar');
+    t.is(error.columnNumber, 12);
+    t.is(error.lineNumber, 2);
+});
+
+
+test('parseScript, jump statement expression syntax error', (t) => {
+    let error = null;
+    try {
+        validateScript(parseScript(`\
+jumpif (@#$) label
+`));
+    } catch (errorTest) {
+        error = errorTest;
+    }
+    t.is(error.message, `\
+Syntax error, line number 1:
+jumpif (@#$) label
+        ^
+`);
+    t.is(error.error, 'Syntax error');
+    t.is(error.line, 'jumpif (@#$) label');
+    t.is(error.columnNumber, 9);
+    t.is(error.lineNumber, 1);
+});
+
+
+test('parseScript, return statement expression syntax error', (t) => {
+    let error = null;
+    try {
+        validateScript(parseScript(`\
+return @#$
+`));
+    } catch (errorTest) {
+        error = errorTest;
+    }
+    t.is(error.message, `\
+Syntax error, line number 1:
+return @#$
+       ^
+`);
+    t.is(error.error, 'Syntax error');
+    t.is(error.line, 'return @#$');
+    t.is(error.columnNumber, 8);
+    t.is(error.lineNumber, 1);
+});
+
+
+test('parseScript, nested function statement error', (t) => {
+    let error = null;
+    try {
+        validateScript(parseScript(`\
+function foo()
+    function bar()
+    endfunction
+endfunction
+`));
+    } catch (errorTest) {
+        error = errorTest;
+    }
+    t.is(error.message, `\
+Nested function definition, line number 2:
+    function bar()
+^
+`);
+    t.is(error.error, 'Nested function definition');
+    t.is(error.line, '    function bar()');
+    t.is(error.columnNumber, 1);
+    t.is(error.lineNumber, 2);
+});
+
+
+test('parseScript, endfunction statement error', (t) => {
+    let error = null;
+    try {
+        validateScript(parseScript(`\
+a = 1
+endfunction
+`));
+    } catch (errorTest) {
+        error = errorTest;
+    }
+    t.is(error.message, `\
+No matching function definition, line number 2:
+endfunction
+^
+`);
+    t.is(error.error, 'No matching function definition');
+    t.is(error.line, 'endfunction');
+    t.is(error.columnNumber, 1);
+    t.is(error.lineNumber, 2);
 });
 
 
@@ -76,7 +326,93 @@ test('parseExpression', (t) => {
             }
         }
     });
-    t.is(evaluateExpression(expr), 7 + 3 * 5);
+});
+
+
+test('parseExpression, unary', (t) => {
+    const expr = parseExpression('!a');
+    t.deepEqual(validateExpression(expr), {
+        'unary': {
+            'op': '!',
+            'expr': {'variable': 'a'}
+        }
+    });
+});
+
+
+test('parseExpression, syntax error', (t) => {
+    let error = null;
+    const exprText = ' @#$';
+    try {
+        parseExpression(exprText);
+    } catch (errorTest) {
+        error = errorTest;
+    }
+    t.is(error.message, `\
+Syntax error:
+${exprText}
+^
+`);
+    t.is(error.line, exprText);
+    t.is(error.columnNumber, 1);
+    t.is(error.lineNumber, null);
+});
+
+
+test('parseExpression, nextText syntax error', (t) => {
+    let error = null;
+    const exprText = 'foo bar';
+    try {
+        parseExpression(exprText);
+    } catch (errorTest) {
+        error = errorTest;
+    }
+    t.is(error.message, `\
+Syntax error:
+${exprText}
+   ^
+`);
+    t.is(error.line, exprText);
+    t.is(error.columnNumber, 4);
+    t.is(error.lineNumber, null);
+});
+
+
+test('parseExpression, syntax error, unmatched parenthesis', (t) => {
+    let error = null;
+    const exprText = '10 * (1 + 2';
+    try {
+        parseExpression(exprText);
+    } catch (errorTest) {
+        error = errorTest;
+    }
+    t.is(error.message, `\
+Unmatched parenthesis:
+${exprText}
+    ^
+`);
+    t.is(error.line, exprText);
+    t.is(error.columnNumber, 5);
+    t.is(error.lineNumber, null);
+});
+
+
+test('parseExpression, function argument syntax error', (t) => {
+    let error = null;
+    const exprText = 'foo(1, 2 3)';
+    try {
+        parseExpression(exprText);
+    } catch (errorTest) {
+        error = errorTest;
+    }
+    t.is(error.message, `\
+Syntax error:
+foo(1, 2 3)
+        ^
+`);
+    t.is(error.line, exprText);
+    t.is(error.columnNumber, 9);
+    t.is(error.lineNumber, null);
 });
 
 
@@ -95,7 +431,6 @@ test('parseExpression, operator precedence', (t) => {
             'right': {'number': 5}
         }
     });
-    t.is(evaluateExpression(expr), 7 * 3 + 5);
 });
 
 
@@ -120,7 +455,6 @@ test('parseExpression, operator precedence 2', (t) => {
             'right': {'number': 1}
         }
     });
-    t.is(evaluateExpression(expr), 2 * 3 + 4 - 1);
 });
 
 
@@ -145,7 +479,6 @@ test('parseExpression, operator precedence 3', (t) => {
             'right': {'number': 1}
         }
     });
-    t.is(evaluateExpression(expr), 2 + 3 + 4 - 1);
 });
 
 
@@ -182,7 +515,6 @@ test('parseExpression, operator precedence 4', (t) => {
             }
         }
     });
-    t.is(evaluateExpression(expr), 1 - 2 + 3 + 4 + 5 * 6);
 });
 
 
@@ -207,7 +539,6 @@ test('parseExpression, operator precedence 5', (t) => {
             }
         }
     });
-    t.is(evaluateExpression(expr), 1 + 2 * 5 / 2);
 });
 
 
@@ -232,7 +563,6 @@ test('parseExpression, operator precedence 6', (t) => {
             }
         }
     });
-    t.is(evaluateExpression(expr), 1 + 2 / 5 * 2);
 });
 
 
@@ -263,7 +593,6 @@ test('parseExpression, operator precedence 7', (t) => {
             }
         }
     });
-    t.is(evaluateExpression(expr), 1 + 2 / 3 / 4 * 5);
 });
 
 
@@ -294,7 +623,6 @@ test('parseExpression, operator precedence 8', (t) => {
             }
         }
     });
-    t.is(evaluateExpression(expr), false);
 });
 
 
@@ -315,7 +643,6 @@ test('parseExpression, group', (t) => {
             'right': {'number': 5}
         }
     });
-    t.is(evaluateExpression(expr), 50);
 });
 
 
@@ -330,33 +657,28 @@ test('parseExpression, group nested', (t) => {
             }
         }
     });
-    t.is(evaluateExpression(expr), 3);
 });
 
 
 test('parseExpression, string literal', (t) => {
     const expr = parseExpression("'abc'");
     t.deepEqual(validateExpression(expr), {'string': 'abc'});
-    t.is(evaluateExpression(expr), 'abc');
 });
 
 
 test('parseExpression, string literal escapes', (t) => {
     const expr = parseExpression("'ab \\'c\\' d\\\\e \\f'");
     t.deepEqual(validateExpression(expr), {'string': "ab 'c' d\\e \\f"});
-    t.is(evaluateExpression(expr), "ab 'c' d\\e \\f");
 });
 
 
 test('parseExpression, string literal double-quote', (t) => {
     const expr = parseExpression('"abc"');
     t.deepEqual(validateExpression(expr), {'string': 'abc'});
-    t.is(evaluateExpression(expr), 'abc');
 });
 
 
 test('parseExpression, string literal double-quote escapes', (t) => {
     const expr = parseExpression('"ab \\"c\\" d\\\\e \\f"');
     t.deepEqual(validateExpression(expr), {'string': 'ab "c" d\\e \\f'});
-    t.is(evaluateExpression(expr), 'ab "c" d\\e \\f');
 });
