397fa7a3d5758cda9d316d92d5351594af25f7a4
Craig Hobbs <craigahobbs@gmail.com>
2022-07-11 16:54:42 -0700

    trim long parser error lines

--- test/testParser.js
+++ test/testParser.js
@@ -55,7 +55,7 @@ test('parseScript, line continuation error', (t) => {
     const error = t.throws(() => {
         parseScript(`\
     fn1(arg1, \\
-    fn2(), 
+    fn2(),
     null))
 `);
     }, {'instanceOf': CalcScriptParserError});
@@ -63,6 +63,84 @@ test('parseScript, line continuation error', (t) => {
 Syntax error, line number 1:
     fn1(arg1, fn2(),
                     ^
+`);
+});
+
+
+test('parseScript, long line error middle', (t) => {
+    const error = t.throws(() => {
+        parseScript(`\
+    reallyLongFunctionName( \\
+        value1 + value2 + value3 + value4 + value5, \\
+        value6 + value7 + value8 + value9 + value10, \\
+        value11 + value12 + value13 + value14 + value15, \\
+        value16 + value17 + value18 + value19 + value20, \\
+        value21 + value22 + value23 + value24 + value25, \\
+        @#$, \\
+        value26 + value27 + value28 + value29 + value30, \\
+        value31 + value32 + value33 + value34 + value35, \\
+        value36 + value37 + value38 + value39 + value40, \\
+        value41 + value42 + value43 + value44 + value45, \\
+        value46 + value47 + value48 + value49 + value50 \\
+   )
+`);
+    }, {'instanceOf': CalcScriptParserError});
+    t.is(error.message, `\
+Syntax error, line number 1:
+...  + value20, value21 + value22 + value23 + value24 + value25, @#$, value26 + value27 + value28 + value29 + value30, value ...
+                                                                ^
+`);
+});
+
+
+test('parseScript, long line error left', (t) => {
+    const error = t.throws(() => {
+        parseScript(`\
+    reallyLongFunctionName( \\
+        @#$, \\
+        value1 + value2 + value3 + value4 + value5, \\
+        value6 + value7 + value8 + value9 + value10, \\
+        value11 + value12 + value13 + value14 + value15, \\
+        value16 + value17 + value18 + value19 + value20, \\
+        value21 + value22 + value23 + value24 + value25, \\
+        value26 + value27 + value28 + value29 + value30, \\
+        value31 + value32 + value33 + value34 + value35, \\
+        value36 + value37 + value38 + value39 + value40, \\
+        value41 + value42 + value43 + value44 + value45, \\
+        value46 + value47 + value48 + value49 + value50 \\
+   )
+`);
+    }, {'instanceOf': CalcScriptParserError});
+    t.is(error.message, `\
+Syntax error, line number 1:
+    reallyLongFunctionName( @#$, value1 + value2 + value3 + value4 + value5, value6 + value7 + value8 + value9 + value10 ...
+                           ^
+`);
+});
+
+
+test('parseScript, long line error right', (t) => {
+    const error = t.throws(() => {
+        parseScript(`\
+    reallyLongFunctionName( \\
+        value1 + value2 + value3 + value4 + value5, \\
+        value6 + value7 + value8 + value9 + value10, \\
+        value11 + value12 + value13 + value14 + value15, \\
+        value16 + value17 + value18 + value19 + value20, \\
+        value21 + value22 + value23 + value24 + value25, \\
+        value26 + value27 + value28 + value29 + value30, \\
+        value31 + value32 + value33 + value34 + value35, \\
+        value36 + value37 + value38 + value39 + value40, \\
+        value41 + value42 + value43 + value44 + value45, \\
+        value46 + value47 + value48 + value49 + value50 \\
+        @#$ \\
+   )
+`);
+    }, {'instanceOf': CalcScriptParserError});
+    t.is(error.message, `\
+Syntax error, line number 1:
+... alue39 + value40, value41 + value42 + value43 + value44 + value45, value46 + value47 + value48 + value49 + value50 @#$ )
+                                                                                                                      ^
 `);
 });
 
