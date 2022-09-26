66f899bacb69e2cedff2a58ed482bb0442f94433
Craig Hobbs <craigahobbs@gmail.com>
2022-09-26 13:24:33 -0700

    update calcscript-mode.el

--- language/calcscript-mode.el
+++ language/calcscript-mode.el
@@ -17,30 +17,35 @@
 ;;; Code:
 (require 'generic-x)
 
-(define-generic-mode 'calcscript-mode
-  '(?#)
-  '(
-    "async"
-    "endfunction"
-    "function"
-    "include"
-    "jump"
-    "jumpif"
-    "return"
-    )
-  '(
-    ("\\(null\\|true\\|false\\)"                       1 font-lock-constant-face)
-    ("\\('\\(\\\\'\\|[^']\\)+'\\)"                     1 font-lock-string-face)
-    ("^\\s-*\\(\\([.]\\|_\\|\\w\\)+\\)\\s-*="          1 font-lock-variable-name-face)
-    ("^\\s-*\\(\\(_\\|\\w\\)+\\s-*:\\)"                1 font-lock-reference-face)
-    ("^\\s-*\\(#+.-*\\s-*\\)$"                         1 font-lock-doc-face)
-    ("^\\(~~~+\\(\\s-*\\(_\\|-\\|\\w\\)+\\)?\\)\\s-*$" 1 font-lock-preprocessor-face)
-    )
-  '(
-    ".mds?\\'"
-    )
-  nil
-  "Major mode for editing CalcScript")
+;;;###autoload
+(defun define-calcscript-mode()
+  (define-generic-mode 'calcscript-mode
+    '(?#)
+    '(
+      "async"
+      "endfunction"
+      "function"
+      "include"
+      "jump"
+      "jumpif"
+      "return"
+      )
+    '(
+      ("\\(null\\|true\\|false\\)"                       1 font-lock-constant-face)
+      ("\\('\\(\\\\'\\|[^']\\)+'\\)"                     1 font-lock-string-face)
+      ("^\\s-*\\(\\([.]\\|_\\|\\w\\)+\\)\\s-*="          1 font-lock-variable-name-face)
+      ("^\\s-*\\(\\(_\\|\\w\\)+\\s-*:\\)"                1 font-lock-reference-face)
+      ("^\\s-*\\(#+.-*\\s-*\\)$"                         1 font-lock-doc-face)
+      ("^\\(~~~+\\(\\s-*\\(_\\|-\\|\\w\\)+\\)?\\)\\s-*$" 1 font-lock-preprocessor-face)
+      )
+    '(
+      ".mds?\\'"
+      )
+    nil
+    "Major mode for editing CalcScript"))
+
+;;;###autoload
+(define-calcscript-mode)
 
 (provide 'calcscript-mode)
 ;;; calcscript-mode.el ends here
