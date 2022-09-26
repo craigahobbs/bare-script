2b50dd560e16a0e2ef99aa5cb9d489216538c782
Craig Hobbs <craigahobbs@gmail.com>
2022-09-26 11:46:09 -0700

    update calcscript-mode.el

--- language/calcscript-mode.el
+++ language/calcscript-mode.el
@@ -13,37 +13,36 @@
 ;;     (url-copy-file "https://craigahobbs.github.io/calcscript/language/calcscript-mode.el" mode-file t)
 ;;     (package-install-file mode-file)
 ;;     (delete-file mode-file)))
-;; (add-to-list 'auto-mode-alist '("\\.mds?\\'" . calcscript-mode))
 
 ;;; Code:
 (require 'generic-x)
 
 ;;;###autoload
-(define-generic-mode 'calcscript-mode
-  '(?#)
-  '(
-    "async"
-    "endfunction"
-    "function"
-    "include"
-    "jump"
-    "jumpif"
-    "return"
-    )
-  '(
-    ("\\(null\\|true\\|false\\)"                       1 font-lock-constant-face)
-    ("\\('\\(\\\\'\\|[^']\\)+'\\)"                     1 font-lock-string-face)
-    ("^\\s-*\\(\\([.]\\|_\\|\\w\\)+\\)\\s-*="          1 font-lock-variable-name-face)
-    ("^\\s-*\\(\\(_\\|\\w\\)+\\s-*:\\)"                1 font-lock-reference-face)
-    ("^\\s-*\\(#+.-*\\s-*\\)$"                         1 font-lock-doc-face)
-    ("^\\(~~~+\\(\\s-*\\(_\\|-\\|\\w\\)+\\)?\\)\\s-*$" 1 font-lock-preprocessor-face)
-    )
-  '(
-    ".md\\'"
-    ".mds\\'"
-    )
-  nil
-  "Major mode for editing CalcScript")
+(defun define-calcscript-mode()
+  (define-generic-mode 'calcscript-mode
+    '(?#)
+    '(
+      "async"
+      "endfunction"
+      "function"
+      "include"
+      "jump"
+      "jumpif"
+      "return"
+      )
+    '(
+      ("\\(null\\|true\\|false\\)"                       1 font-lock-constant-face)
+      ("\\('\\(\\\\'\\|[^']\\)+'\\)"                     1 font-lock-string-face)
+      ("^\\s-*\\(\\([.]\\|_\\|\\w\\)+\\)\\s-*="          1 font-lock-variable-name-face)
+      ("^\\s-*\\(\\(_\\|\\w\\)+\\s-*:\\)"                1 font-lock-reference-face)
+      ("^\\s-*\\(#+.-*\\s-*\\)$"                         1 font-lock-doc-face)
+      ("^\\(~~~+\\(\\s-*\\(_\\|-\\|\\w\\)+\\)?\\)\\s-*$" 1 font-lock-preprocessor-face)
+      )
+    '(
+      ".mds?\\'"
+      )
+    nil
+    "Major mode for editing CalcScript"))
 
 (provide 'calcscript-mode)
 ;;; calcscript-mode.el ends here
