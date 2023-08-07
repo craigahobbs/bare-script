d21e0ac1d4dd4afa9e9e282d5ef93ba40d36473b
Craig Hobbs <craigahobbs@gmail.com>
2023-08-07 10:12:21 -0700

    bare-script 2.0.2

---
+++ language/barescript-mode.el
@@ -0,0 +1,97 @@
+;;; barescript-mode.el --- Major mode for editing BareScript files
+
+;; Version: 0.6
+
+;;; Commentary:
+
+;; To install, add the following to your .emacs file:
+
+;; (package-initialize)
+;;
+;; (unless (package-installed-p 'barescript-mode)
+;;   (let ((mode-file (make-temp-file "barescript-mode")))
+;;     (url-copy-file "https://craigahobbs.github.io/bare-script/language/barescript-mode.el" mode-file t)
+;;     (package-install-file mode-file)
+;;     (delete-file mode-file)))
+;; (add-to-list 'auto-mode-alist '("\\.\\(?:[Cc]alc-?[Ss]cript\\|mds\\)\\'" . barescript-mode))
+
+;;; Code:
+
+(defconst barescript-keywords
+  (regexp-opt
+   '("async" "break" "continue" "else" "elif" "endfor" "endfunction"
+     "endif" "endwhile" "false" "for" "function" "if" "in" "include"
+     "jump" "jumpif" "null" "return" "true" "while")
+   'symbols)
+  )
+
+(defconst barescript-font-lock-keywords
+  (list
+   (cons barescript-keywords 'font-lock-keyword-face)
+
+   ;; Rule for variable assignment highlighting
+   '("^\\s-*\\([_A-Za-z][_A-Za-z0-9]*\\)\\s-*=" 1 'font-lock-variable-name-face)
+
+   ;; Rule for label highlighting
+   '("^\\s-*\\([_A-Za-z][_A-Za-z0-9]*\\)\\s-*:\\s-*$" 1 'font-lock-constant-face)
+   )
+  )
+
+(defun barescript-open-language-documentation ()
+  "Open BareScript language documentation"
+  (interactive)
+  (browse-url "https://craigahobbs.github.io/bare-script/language/")
+  )
+
+(defun barescript-open-markdownup-library ()
+  "Open MarkdownUp library documentation"
+  (interactive)
+  (browse-url "https://craigahobbs.github.io/markdown-up/library/")
+  )
+
+(defun barescript-open-markdownup-function ()
+  "Open BareScript Library documentation for the function at point"
+  (interactive)
+  (let* ((library-url "https://craigahobbs.github.io/markdown-up/library/#var.vName='%s'")
+         (word-at-point (thing-at-point 'symbol))
+         (formatted-url (format library-url word-at-point)))
+    (if word-at-point (browse-url formatted-url)
+      (message "No valid BareScript function at point"))
+    )
+  )
+
+;;;###autoload
+(define-derived-mode barescript-mode prog-mode "BareScript"
+  "Major mode for editing BareScript files"
+
+  ; Change single quote syntax to behave like double quotes
+  (modify-syntax-entry ?' "\"" barescript-mode-syntax-table)
+
+  ; Ensure double quotes are treated as string delimiters
+  (modify-syntax-entry ?\" "\"" barescript-mode-syntax-table)
+
+  ; Ensure backslashes are treated as escape characters
+  (modify-syntax-entry ?\\ "\\" barescript-mode-syntax-table)
+
+  ; Specify that comments start with '#'
+  (modify-syntax-entry ?# "<" barescript-mode-syntax-table)
+
+  ; Specify that comments end with a newline
+  (modify-syntax-entry ?\n ">" barescript-mode-syntax-table)
+
+  ;; Set comment-related variables
+  (setq-local comment-start "#")
+  (setq-local comment-start-skip "#+\\s-*")
+
+  ;; Apply font-lock rules for syntax highlighting
+  (setq-local font-lock-defaults '(barescript-font-lock-keywords))
+
+  ;; Bind the key for browsing documentation
+  (define-key barescript-mode-map (kbd "C-c C-l") 'barescript-open-language-documentation)
+  (define-key barescript-mode-map (kbd "C-c C-m") 'barescript-open-markdownup-library)
+  (define-key barescript-mode-map (kbd "C-c C-f") 'barescript-open-markdownup-function)
+  )
+
+(provide 'barescript-mode)
+
+;;; barescript-mode.el ends here
